422ce1e69e3dff27836aa961393855d0a3ebd48f
mmsqe <tqd0800210105@gmail.com>
2025-10-09 11:32:13 +0800

    Problem: ledger flag not work with coin type 60

--- cmd/mantrachaind/cmd/root.go
+++ cmd/mantrachaind/cmd/root.go
@@ -18,7 +18,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/auth/tx"
 	txmodule "github.com/cosmos/cosmos-sdk/x/auth/tx/config"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-	"github.com/cosmos/evm/crypto/hd"
+	"github.com/cosmos/evm/crypto/keyring"
 	"github.com/spf13/cobra"
 )
 
@@ -57,7 +57,7 @@ func NewRootCmd() *cobra.Command {
 		WithBroadcastMode(flags.BroadcastSync).
 		WithHomeDir(app.DefaultNodeHome).
 		WithViper("MANTRA").
-		WithKeyringOptions(hd.EthSecp256k1Option()).
+		WithKeyringOptions(keyring.Option()).
 		WithLedgerHasProtobuf(true)
 
 	rootCmd := &cobra.Command{
